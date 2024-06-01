9e44af09ca4616e9ee377b97138872407c56fbb8
SOUMYADEEP SADHUKHAN <123949000+Soumyadeep0125@users.noreply.github.com>
2024-06-01 11:30:46 +0530

    first commit

---
+++ src/config/gemini.js
@@ -0,0 +1,57 @@
+
+
+import {
+  GoogleGenerativeAI,
+  HarmCategory,
+  HarmBlockThreshold,
+} from "@google/generative-ai"
+
+const apiKey = "AIzaSyAkU1K--lSV6zdK_jjNEZ6lvHDn9PUL2s8";
+const genAI = new GoogleGenerativeAI(apiKey);
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.0-pro",
+});
+
+const generationConfig = {
+  temperature: 0.9,
+  topP: 1,
+  topK: 0,
+  maxOutputTokens: 2048,
+  responseMimeType: "text/plain",
+};
+
+const safetySettings = [
+  {
+    category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+    threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+  },
+  {
+    category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+    threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+  },
+  {
+    category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+    threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+  },
+  {
+    category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+    threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+  },
+];
+
+async function run(prompt) {
+  const chatSession = model.startChat({
+    generationConfig,
+    safetySettings,
+    history: [
+    ],
+  });
+
+  const result = await chatSession.sendMessage(prompt);
+  const response = result.response;
+  console.log(result.response.text());
+  return response.text();
+}
+
+export default run;
